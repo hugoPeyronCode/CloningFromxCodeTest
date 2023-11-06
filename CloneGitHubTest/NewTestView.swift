8713e1854de2af8175d8ff533bcab0208fc63d57
Hugo Peyron <hugopeyron@gmail.com>
2023-11-06 16:10:36 +0100

    New Test View Created

---
+++ CloneGitHubTest/NewTestView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTestView.swift
+//  CloneGitHubTest
+//
+//  Created by Hugo Peyron on 06/11/2023.
+//
+
+import SwiftUI
+
+struct NewTestView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    NewTestView()
+}
