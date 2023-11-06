096ad5d2aef487e3eb2cecd48915749019a86089
Hugo Peyron <hugopeyron@gmail.com>
2023-11-06 16:06:10 +0100

    Initial Commit

---
+++ CloneGitHubTest/CloneGitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneGitHubTestApp.swift
+//  CloneGitHubTest
+//
+//  Created by Hugo Peyron on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CloneGitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
